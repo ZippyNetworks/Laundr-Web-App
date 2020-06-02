1c133ad6518631466a4d964c32bd5d8bee797a24
Jack Zheng <jackzheng10@yahoo.com>
2020-06-02 01:39:34 -0400

    Some styling to subscription page

--- client/src/components/User/Subscription/Subscription.js
+++ client/src/components/User/Subscription/Subscription.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { withStyles } from "@material-ui/core";
+import { withStyles, Grid, Typography, Paper } from "@material-ui/core";
 import PropTypes from "prop-types";
 import subscriptionStyles from "../../../styles/User/Subscription/subscriptionStyles";
+import sectionBorder from "../../../images/UserDashboard/sectionBorder.png";
 
 class Subscription extends Component {
   constructor(props) {
@@ -11,7 +12,65 @@ class Subscription extends Component {
   render() {
     const classes = this.props.classes;
 
-    return <h1>this is a sub page</h1>;
+    return (
+      <React.Fragment>
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          justify="center"
+          alignItems="center" /*main page column*/
+          style={{
+            paddingTop: 8,
+            backgroundColor: "#21d0e5",
+          }}
+        >
+          <Grid item>
+            <Typography variant="h1" className={classes.componentName}>
+              Subscription
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          justify="center"
+          alignItems="center" /*main page column*/
+        >
+          <img
+            src={sectionBorder}
+            style={{
+              width: "100%",
+              height: "100%",
+              paddingTop: 8,
+              paddingBottom: 15,
+            }}
+            alt="Section border"
+          />
+        </Grid>
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          justify="center"
+          alignItems="center" /*main page column*/
+        >
+          <Grid item>
+            <h1>1</h1>
+          </Grid>
+          <Grid item>
+            <h1>2</h1>
+          </Grid>
+          <Grid item>
+            <h1>3</h1>
+          </Grid>
+          <Grid item>
+            <h1>4</h1>
+          </Grid>
+        </Grid>
+      </React.Fragment>
+    );
   }
 }
 
--- client/src/styles/User/Subscription/subscriptionStyles.js
+++ client/src/styles/User/Subscription/subscriptionStyles.js
@@ -1,1 +1,11 @@
-export default (theme) => ({});
+export default (theme) => ({
+  welcomeText: {
+    color: "white",
+    textAlign: "center",
+    padding: 10,
+  },
+  componentName: {
+    color: "white",
+    textAlign: "center",
+  },
+});
